0620da562a7ad077072ab957a3b11431bc7bd8ee
Matthew Lakatos <24mlaka@gmail.com>
2026-04-08 13:56:00 +0100

    Create sweep_runner.py

---
+++ experiments/sweep_runner.py
@@ -0,0 +1,6 @@
+# load grid of configs (list of dicts)
+# for each config:
+#   for seed in seeds:
+#       call run_experiment.main(config with seed) or import train() and run in-process
+#       collect tau_grok and path to CSV
+# aggregate results into a master CSV for analysis
